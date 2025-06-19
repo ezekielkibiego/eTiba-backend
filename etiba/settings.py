a4fadace710cb60fabc964ba391a3986aa7beca7
Ezekiel Kibiego <kibiezekiel@gmail.com>
2025-06-19 17:59:21 +0300

    Revert "feat(auth): Implement email-based authentication and verification flow"
    This reverts commit 229753de94a2a80f078b7a88ce760f81e299d92a.

--- etiba/settings.py
+++ etiba/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-c)6&(7+^)6_v@za9oaf#$fny__*lm=6rvvpp+jw)8q2nxq^*i^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -142,7 +142,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Africa/Nairobi'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
